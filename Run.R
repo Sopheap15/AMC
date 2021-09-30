61be2882dba42050e6682be00d5f1420e2091393
Sopheap <sopheap15@gmail.com>
2021-09-30 19:35:10 +0700

    bb and SR

--- Run.R
+++ Run.R
@@ -1,10 +1,14 @@
-# load file----
-source("code/Load_file.R",local = knitr::knit_global())
+# Run-----
+# load file
+#source("code/Load_file_SR.R",local = knitr::knit_global()) # load file from Siem Reap
 
-# render report----
+source("code/Load_file_BB.R",local = knitr::knit_global()) # load file from Battambang
+
+# render report
 rmarkdown::render(
   input = "code/AMC.Rmd",
   output_file = paste0("../Output/AMC Report ",Sys.Date(),".html")) 
 
-# clear objects----
+# clear objects
 rm(list = ls())
+
